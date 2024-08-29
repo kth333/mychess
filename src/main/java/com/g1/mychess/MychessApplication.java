7b95323f9bf4f6b1f306c42363cb30b73cf9aca6
Kang Taeho <kangtaeho2456@gmail.com>
2024-08-29 11:34:40 +0800

    Initial Spring Boot Setup

---
+++ src/main/java/com/g1/mychess/MychessApplication.java
@@ -0,0 +1,13 @@
+package com.g1.mychess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MychessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MychessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/g1/mychess/MychessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g1.mychess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MychessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
