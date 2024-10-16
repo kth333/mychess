d8af84e661191bb8418d30f386de69ffc6181850
ppv-1 <virus9320@gmail.com>
2024-10-16 14:20:56 +0800

    Update package-lock.json, Add styling for Home.jsx and Tournaments.jsx

--- frontend/src/components/tournaments/Tournaments.jsx
+++ frontend/src/components/tournaments/Tournaments.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
 import { CalendarIcon, TrophyIcon, UsersIcon } from "lucide-react";
 import withNavigateandLocation from '../withNavigateandLocation';
-import Pagination from "../ui/Pagination"  // Import the Pagination component
+import Pagination from "../ui/Pagination";  // Import the Pagination component
 import TournamentService from '../../services/TournamentService';
 
 class Tournaments extends Component {
@@ -14,12 +14,26 @@ class Tournaments extends Component {
       currentPage: 1,
       tournamentsPerPage: 9,
       tournaments: [],
+      mouseX: 0,
+      mouseY: 0,
     };
   }
 
   componentDidMount() {
     this.fetchData();
+    window.addEventListener('mousemove', this.handleMouseMove);
   }
+
+  componentWillUnmount() {
+    window.removeEventListener('mousemove', this.handleMouseMove);
+  }
+
+  handleMouseMove = (event) => {
+    this.setState({
+      mouseX: event.clientX,
+      mouseY: event.clientY,
+    });
+  };
 
   fetchData = async () => {
     try {
@@ -54,7 +68,7 @@ class Tournaments extends Component {
   };
 
   render() {
-    const { tournaments, searchQuery, currentPage, tournamentsPerPage } = this.state;
+    const { tournaments, searchQuery, currentPage, tournamentsPerPage, mouseX, mouseY } = this.state;
 
     const filteredTournaments = this.filterTournaments(tournaments);
     const paginatedTournaments = this.paginateTournaments(filteredTournaments);
@@ -62,10 +76,32 @@ class Tournaments extends Component {
     const userRole = sessionStorage.getItem("role");
     const isAdmin = userRole === 'ROLE_ADMIN';
 
+    // Define gradients based on the theme
+    let gradient;
+
+    // Define the opacity limits
+    const minOpacity = 0.4; 
+    const maxOpacity = 0.9; 
+
+    // Calculate the mouse positions as ratios
+    const mouseXRatio = mouseX / window.innerWidth;
+    const mouseYRatio = mouseY / window.innerHeight;
+
+    // Constrain the opacity values using the defined limits
+    const opacityX = minOpacity + (maxOpacity - minOpacity) * mouseXRatio;
+    const opacityY = minOpacity + (maxOpacity - minOpacity) * mouseYRatio;
+
+    const theme = sessionStorage.getItem('theme');
+    if (theme === 'light') {
+      gradient = `linear-gradient(to right, rgba(34, 34, 34, ${opacityX}), rgba(0, 51, 102, ${opacityY}))`;
+    } else {
+      gradient = `linear-gradient(to right, rgba(0, 128, 255, ${opacityX}), rgba(128, 0, 255, ${opacityY}))`;
+    }
+
     return (
       <div className="flex flex-col min-h-screen">
         <main className="flex-grow">
-          <section className="py-20 px-6 text-center bg-gradient-to-r from-blue-500 to-purple-600 text-white">
+          <section className="py-20 px-6 text-center text-white"  style={{ background: gradient }}>
             <h1 className="text-4xl font-bold mb-4">All Tournaments</h1>
             <p className="text-xl mb-8">Explore and view details of all available chess tournaments</p>
             <div className="mb-8">
@@ -83,43 +119,42 @@ class Tournaments extends Component {
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
               {paginatedTournaments.map((tournament) => (
                 <Card key={tournament.id} className="h-full flex flex-col">
-                <CardHeader>
-                  <CardTitle>{tournament.name}</CardTitle>
-                </CardHeader>
-              
-                {/* Use flex-grow to push the bottom section down */}
-                <CardContent className="flex flex-col flex-grow">
-                  <div className="flex-grow"></div> {/* Spacer to push bottom content down */}
-              
-                  {/* Grouped participants, rating, and button at the bottom */}
-                  <div className="space-y-4">
-                    <p className="flex items-center">
-                      <CalendarIcon className="inline-block mr-2 h-4 w-4" />
-                      {new Date(tournament.startDateTime).toLocaleDateString()} - {new Date(tournament.endDateTime).toLocaleDateString()}
-                    </p>
-                    <p className="flex items-center">
-                      <UsersIcon className="mr-2 h-4 w-4" />
-                      {tournament.participants.length} participants
-                    </p>
-                    <p className="flex items-center">
-                      <TrophyIcon className="mr-2 h-4 w-4 text-yellow-500" />
-                      Rating Range: {tournament.minRating} - {tournament.maxRating}
-                    </p>
-              
-                    <div>
-                      {isAdmin ? (
-                        <Link className="btn btn-primary w-full font-bold" to={`/tournaments/${tournament.name}`}>
-                          Manage
-                        </Link>
-                      ) : (
-                        <Link className="btn btn-primary w-full font-bold" to={`/tournaments/${tournament.name}`}>
-                          View Details
-                        </Link>
-                      )}
+                  <CardHeader>
+                    <CardTitle>{tournament.name}</CardTitle>
+                  </CardHeader>
+                  
+                  <CardContent className="flex flex-col flex-grow">
+                    <div className="flex-grow"></div> {/* Spacer to push bottom content down */}
+                    
+                    {/* Grouped participants, rating, and button at the bottom */}
+                    <div className="space-y-4">
+                      <p className="flex items-center">
+                        <CalendarIcon className="inline-block mr-2 h-4 w-4" />
+                        {new Date(tournament.startDateTime).toLocaleDateString()} - {new Date(tournament.endDateTime).toLocaleDateString()}
+                      </p>
+                      <p className="flex items-center">
+                        <UsersIcon className="mr-2 h-4 w-4" />
+                        {tournament.participants.length} participants
+                      </p>
+                      <p className="flex items-center">
+                        <TrophyIcon className="mr-2 h-4 w-4 text-yellow-500" />
+                        Rating Range: {tournament.minRating} - {tournament.maxRating}
+                      </p>
+                      
+                      <div>
+                        {isAdmin ? (
+                          <Link className="btn btn-primary w-full font-bold" to={`/tournaments/${tournament.name}`}>
+                            Manage
+                          </Link>
+                        ) : (
+                          <Link className="btn btn-primary w-full font-bold" to={`/tournaments/${tournament.name}`}>
+                            View Details
+                          </Link>
+                        )}
+                      </div>
                     </div>
-                  </div>
-                </CardContent>
-              </Card>
+                  </CardContent>
+                </Card>
               ))}
             </div>
             <Pagination
@@ -130,8 +165,6 @@ class Tournaments extends Component {
             />
           </section>
         </main>
-
-        
       </div>
     );
   }
