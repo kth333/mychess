ddf40660775a479ab18f5521bfbbcc00bdc537d6
Yuhanna <yuhanna.mi.2023@scis.smu.edu.sg>
2024-10-16 23:17:59 +0800

    Revert "Add testing for admin"
    This reverts commit 813ab318392353ac3cfd44642de8a4f91403c379.

--- admin-service/src/test/java/com/g1/mychess/admin/exception/AdminNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.g1.mychess.admin.exception;
-
-public class AdminNotFoundException extends RuntimeException {
-    public AdminNotFoundException(String message) {
-        super(message);
-    }
-}
--- admin-service/src/test/java/com/g1/mychess/admin/exception/InvalidBlacklistOperationException.java
+++
@@ -1,7 +0,0 @@
-package com.g1.mychess.admin.exception;
-
-public class InvalidBlacklistOperationException extends RuntimeException {
-    public InvalidBlacklistOperationException(String message) {
-        super(message);
-    }
-}
