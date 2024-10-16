562636606c77ce739189d87efafbf0a8e4876955
ppv-1 <virus9320@gmail.com>
2024-10-17 01:30:55 +0800

    Add blacklist, whitelist, remove player from tournament

--- frontend/src/services/TournamentService.js
+++ frontend/src/services/TournamentService.js
@@ -18,6 +18,10 @@ class TournamentService {
       return await TournamentAPI.get(`/tournaments/public/get/${tournamentName}`);
     }
 
+    async getTournamentById(tournamentId) {
+      return await TournamentAPI.get(`/tournaments/public/get/id/${tournamentId}`);
+    }
+
     async signUp(tournamentId) {
       return await ProtectedTournamentAPI.post(`/tournaments/player/signup/${tournamentId}`);
     }
@@ -32,6 +36,14 @@ class TournamentService {
 
     async completeTournament(tournamentId) {
       return await ProtectedTournamentAPI.post(`/tournaments/admin/complete/${tournamentId}`);
+    }
+
+    async leaveTournament(tournamentId) {
+      return await ProtectedTournamentAPI.delete(`/tournaments/player/leave/${tournamentId}`);
+    }
+
+    async removePlayerFromTournament(tournamentId, playerId) {
+      return await ProtectedTournamentAPI.delete(`/tournaments/admin/remove/${tournamentId}/${playerId}`);
     }
 
     
