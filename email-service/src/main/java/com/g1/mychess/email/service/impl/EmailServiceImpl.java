22123ce60bdb6e5cbe49a6d664b5e42d4ecde9f7
Yornhub <162811719+Yornhub@users.noreply.github.com>
2024-11-12 20:59:58 +0800

    Completed Integration Tests for EmailService

--- email-service/src/main/java/com/g1/mychess/email/service/impl/EmailServiceImpl.java
+++ email-service/src/main/java/com/g1/mychess/email/service/impl/EmailServiceImpl.java
@@ -20,7 +20,7 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class EmailServiceImpl implements EmailService {
-
+    @Autowired
     private final JavaMailSender mailSender;
 
     // URL for the authentication service, injected from application properties
