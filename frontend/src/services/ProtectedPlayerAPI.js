dd6e639ba297ccaa422b94e422f1aefc8384eff6
Kang Taeho <kangtaeho2456@gmail.com>
2024-11-12 14:45:16 +0800

    Configure https

--- frontend/src/services/ProtectedPlayerAPI.js
+++ frontend/src/services/ProtectedPlayerAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = "http://54.151.148.156:8081/api/v1";
+const API_BASE_URL = "https://54.151.148.156:8081/api/v1";
 // const API_BASE_URL = "http://localhost:8081/api/v1";
 
 
