efe37e23055cb1a77d2c81aec1557b2e5e90b2a5
Kang Taeho <kangtaeho2456@gmail.com>
2024-11-12 19:18:08 +0800

    Update dockerfiles

--- frontend/src/services/ProtectedAdminAPI.js
+++ frontend/src/services/ProtectedAdminAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = "https://13.212.177.252:8084/api/v1";
+const API_BASE_URL = "https://54.151.148.156:8084/api/v1";
 // const API_BASE_URL = "http://localhost:8084/api/v1";
 
 
