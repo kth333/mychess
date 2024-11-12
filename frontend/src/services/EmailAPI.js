df2a35902e819bd5aa7722620e68d1ba22645162
Kang Taeho <kangtaeho2456@gmail.com>
2024-11-12 20:07:45 +0800

    Update urls

--- frontend/src/services/EmailAPI.js
+++ frontend/src/services/EmailAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = "https://13.212.222.10:8085/api/v1";
+const API_BASE_URL = "https://mychesss.com/api/v1";
 // const API_BASE_URL = "http://localhost:8085/api/v1";
 
 
--- frontend/src/services/TournamentAPI.js
+++ frontend/src/services/TournamentAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = "https://54.169.233.165:8083/api/v1";
+const API_BASE_URL = "https://mychesss.com/api/v1";
 // const API_BASE_URL = "http://localhost:8083/api/v1";
 
 
