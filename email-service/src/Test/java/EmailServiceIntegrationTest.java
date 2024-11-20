84ad64486b1f5532bf30ad311d29c61e7e1fbaeb
Yornhub <162811719+Yornhub@users.noreply.github.com>
2024-11-20 20:35:38 +0800

    Documentaton for EmailService Integration test

--- email-service/src/Test/java/EmailServiceIntegrationTest.java
+++ email-service/src/Test/java/EmailServiceIntegrationTest.java
@@ -31,6 +31,26 @@ public class EmailServiceIntegrationTest {
 
     private static GreenMail smtpServer;
 
+    /**
+     * Sets up the test environment before each test execution.
+     *
+     * This method initializes and starts a GreenMail SMTP server for testing purposes
+     * on localhost at port 2525. It also configures a JavaMailSenderImpl instance to
+     * send emails through the GreenMail server. An EmailServiceImpl instance is
+     * then configured with the JavaMailSenderImpl.
+     *
+     * This setup ensures that any email interactions performed by the
+     * EmailServiceImpl during tests are done through the fake GreenMail server, thus
+     * avoiding any real email transmissions.
+     *
+     * Email Service Layer is relatively simple without many failure cases.
+     * Validity of local and domain part of email covered by auth-service.
+     *      And correctness of logic tested by AuthServiceUnitTests
+     *
+     * Existence of username and email already in db, is handled by player-service.
+     * We'll just test the "end-points"
+     */
+
     @BeforeEach
     public void setUp() {
         // Email Server for testing
@@ -53,9 +73,13 @@ public class EmailServiceIntegrationTest {
     }
 
     /**
-     * Email Service Layer is relatively simple without many failure cases.
-     * Validity of local and domain part of email covered in auth service unit tests.
-     * Checks for whether username and email already exist in db, handled by player-service.
+     * Tests the successful sending of a verification email.
+     *
+     * This test verifies that the email is sent to the correct recipient, and the content and subject
+     * of the email contain the expected verification token and relevant text.
+     *
+     * @throws MessagingException if there is a failure in the messaging components.
+     * @throws IOException if there is an input-output failure during the email sending process.
      */
 
     @Test
@@ -74,9 +98,17 @@ public class EmailServiceIntegrationTest {
         String emailSubject = receivedMessages[0].getSubject();
         assertTrue(emailSubject.contains("Verification"), "Email body does not contain \"Verification\".");
         assertTrue(emailSubject.contains("MyChess"), "Email Subject does not contain \"MyChess\".");
-
     }
 
+    /**
+     * Tests the successful sending of a blacklist email.
+     *
+     * This test verifies that the email is sent to the correct recipient, and the content and subject
+     * of the email contain the appropriate reason for blacklisting and the ban duration.
+     *
+     * @throws MessagingException if there is a failure in the messaging components.
+     * @throws IOException if there is an input-output failure during the email sending process.
+     */
     @Test
     void test_sendBlacklistEmail_Success() throws MessagingException, IOException {
         String to = "recipient@example.com";
@@ -94,9 +126,17 @@ public class EmailServiceIntegrationTest {
         String emailSubject = receivedMessages[0].getSubject();
         assertTrue(emailSubject.contains("Blacklist"), "Email body does not contain \"Blacklist\".");
         assertTrue(emailSubject.contains("MyChess"), "Email Subject does not contain \"MyChess\".");
-
     }
 
+    /**
+     * Tests the successful sending of a whitelist email.
+     *
+     * This test verifies that the email is sent to the correct recipient, and the content and subject
+     * of the email contain the expected whitelist reason and relevant text.
+     *
+     * @throws MessagingException if there is a failure in the messaging components.
+     * @throws IOException if there is an input-output failure during the email sending process.
+     */
     @Test
     void test_sendWhitelistEmail_Success() throws MessagingException, IOException {
 
@@ -115,9 +155,17 @@ public class EmailServiceIntegrationTest {
         assertTrue(emailContent.contains(reason), "Email body does not contain the verification token.");
         assertTrue(emailSubject.contains("Whitelist"), "Email body does not contain \"Whitelist\".");
         assertTrue(emailSubject.contains("MyChess"), "Email Subject does not contain \"MyChess\".");
-
     }
 
+    /**
+     * Tests the successful sending of a password reset email.
+     *
+     * This test verifies that the email is sent to the correct recipient and the content
+     * and subject of the email contain the expected password reset token and relevant text.
+     *
+     * @throws MessagingException if there is a failure in the messaging components.
+     * @throws IOException if there is an input-output failure during the email sending process.
+     */
     @Test
     void test_sendPasswordResetEmail_Success() throws MessagingException, IOException {
 
@@ -136,7 +184,6 @@ public class EmailServiceIntegrationTest {
         assertTrue(emailContent.contains(resetToken), "Email body does not contain the reset token.");
         assertTrue(emailSubject.contains("Password Reset"), "Email body does not contain \"Password Reset\".");
         assertTrue(emailSubject.contains("MyChess"), "Email Subject does not contain \"MyChess\".");
-
     }
 
 }
